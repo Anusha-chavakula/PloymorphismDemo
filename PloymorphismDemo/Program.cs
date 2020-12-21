5efba27e245184b3e4b4f4d6585d0b5f876dd587
Anusha Chavakula <achavakula@rythmos.com>
2020-12-21 17:29:04 +0530

    Added program

--- PloymorphismDemo/Program.cs
+++ PloymorphismDemo/Program.cs
@@ -2,11 +2,38 @@
 
 namespace PloymorphismDemo
 {
+    class Area
+    {
+        public void area(int s)
+        {
+            Console.WriteLine("Area of Square is " + s * s);
+        }
+        public void area(int l,int b)
+        {
+            Console.WriteLine("Area of Rectangle is " + l * b);
+        }
+        public void area(float f,int b,int h)
+        {
+            Console.WriteLine("Area of Triangle is " + f * b * h);
+        }
+        public void area(float f,int r)
+        {
+            Console.WriteLine("Area of Circle is " + f * r * r);
+        }
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Area a = new Area();
+            a.area(5, 3);
+            a.area(0.5f, 10);
+            a.area(6);
+            a.area(0.5f, 8, 5);
+            Console.ReadKey();
+
+
         }
     }
 }
